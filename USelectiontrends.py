677442d64be97f546e59b0f967c699875b60a006
NavneetNandan <navneet.nandan8@gmail.com>
2016-11-03 00:17:44 +0530

    added distribution of original vs retweeted tweets

--- USelectiontrends.py
+++ USelectiontrends.py
@@ -4,14 +4,29 @@ import pandas as pd
 
 app = Flask(__name__)
 
-DATABASE_NAME = 'uselectiontrends'
-client = MongoClient()
-db = client[DATABASE_NAME]
-
 
 @app.route('/')
 def hello_world():
-    return render_template("USelections.html", hashtags=top10_hashtags_in_election())
+    DATABASE_NAME = 'uselectiontrends'
+    client = MongoClient()
+    db = client[DATABASE_NAME]
+    tweets = db['tweets'].find()
+    counted_retweeted = 0
+    counted_original=0
+    for tweet in tweets:
+        if (tweet.__contains__("retweeted_status")):
+            # print("retweeted")
+            counted_retweeted = counted_retweeted + 1
+        else:
+            # print("original")
+            counted_original += 1
+    print(counted_retweeted)
+    print(counted_original)
+    original_tweet_percent=(counted_original/(counted_original+counted_retweeted))*100
+    retweeted_tweet_percent=(counted_retweeted/(counted_original+counted_retweeted))*100
+    return render_template("USelections.html", hashtags=top10_hashtags_in_election(),
+                           original_tweet_percent=original_tweet_percent,
+                           retweeted_tweet_percent=retweeted_tweet_percent)
 
 
 def top10_hashtags_in_election():
@@ -19,6 +34,9 @@ def top10_hashtags_in_election():
 
     :rtype: list
     """
+    DATABASE_NAME = 'uselectiontrends'
+    client = MongoClient()
+    db = client[DATABASE_NAME]
     tweets = db['tweets'].find()
     all_hashtags_map = {}
     for tweet in tweets:
@@ -30,7 +48,7 @@ def top10_hashtags_in_election():
             else:
                 all_hashtags_map[hashtag_text] = 1
     all_hashtags_pandas_series = pd.Series(all_hashtags_map)
-    all_hashtags_pandas_series.sort(ascending=False)
+    all_hashtags_pandas_series.sort_values(inplace=True,ascending=False)
     top10_hashtags = all_hashtags_pandas_series[0:10].keys()
     return top10_hashtags
 
