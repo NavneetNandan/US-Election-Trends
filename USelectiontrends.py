c4aeac6ba0d003688c094824fa6f68e7f10fe992
NavneetNandan <navneet.nandan8@gmail.com>
2016-11-03 12:01:10 +0530

    added geoplotting of places of tweets

--- USelectiontrends.py
+++ USelectiontrends.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+import matplotlib.pyplot as plt
 import pandas as pd
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
 def hello_world():
@@ -12,17 +13,30 @@ def hello_world():
     db = client[DATABASE_NAME]
     tweets = db['tweets'].find()
     counted_retweeted = 0
-    counted_original=0
+    counted_original = 0
+    favourite_counts=[]
+    places_of_tweets={}
     for tweet in tweets:
+        favourite_counts.append(tweet.get("favorite_count"))
         if (tweet.__contains__("retweeted_status")):
             counted_retweeted = counted_retweeted + 1
         else:
             counted_original += 1
-    original_tweet_percent=(counted_original/(counted_original+counted_retweeted))*100
-    retweeted_tweet_percent=(counted_retweeted/(counted_original+counted_retweeted))*100
+        if tweet['place']!=None:
+            tweet_place_full_name=tweet['place']['full_name']
+            if places_of_tweets.__contains__(tweet_place_full_name):
+                places_of_tweets[tweet_place_full_name]+=1
+            else:
+                places_of_tweets[tweet_place_full_name]=1
+    print(places_of_tweets)
+    original_tweet_percent = (counted_original / (counted_original + counted_retweeted)) * 100
+    retweeted_tweet_percent = (counted_retweeted / (counted_original + counted_retweeted)) * 100
+    # print(favourite_counts)
+    # plt.hist(favourite_counts)
+    # plt.show()
     return render_template("USelections.html", hashtags=top10_hashtags_in_election(),
                            original_tweet_percent=original_tweet_percent,
-                           retweeted_tweet_percent=retweeted_tweet_percent)
+                           retweeted_tweet_percent=retweeted_tweet_percent,places_of_tweets=places_of_tweets)
 
 
 def top10_hashtags_in_election():
@@ -44,7 +58,7 @@ def top10_hashtags_in_election():
             else:
                 all_hashtags_map[hashtag_text] = 1
     all_hashtags_pandas_series = pd.Series(all_hashtags_map)
-    all_hashtags_pandas_series.sort_values(inplace=True,ascending=False)
+    all_hashtags_pandas_series.sort_values(inplace=True, ascending=False)
     top10_hashtags = all_hashtags_pandas_series[0:10].keys()
     return top10_hashtags
 
